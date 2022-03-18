240517ae6319f30f833f85be060f06d21b84453e
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-03-17 21:00:57 -0300

    Update path

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinew'
 
 sinew = Sinew.new(output: 'output.csv', expires: 60)
 
-get '/trends' do
+get '/api/trends' do
   response = sinew.get 'https://clarinete.seppo.com.ar/api/trends'
   response.body
 end
